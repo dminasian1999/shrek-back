3835af4f95b8044956828a0f42362215b1bde624
LENOVO <d.minasian1999@gmail.com>
2025-08-08 11:56:48 +0300

    added ord

--- src/main/java/dev/shrekback/accounting/model/OrderItem.java
+++ src/main/java/dev/shrekback/accounting/model/OrderItem.java
@@ -1,5 +1,12 @@
 package dev.shrekback.accounting.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItem {
     String productId;
     String productName;
